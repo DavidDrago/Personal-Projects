0aac6f0b130730c2931e5a1ec72fe3612bc1b4da
Mukurupu Raju <v-mukraju@microsoft.com>
2026-01-08 13:46:38 +0530

    Written tests in PageObjectModel

---
+++ Selenium/Selenium Testing with Java/src/test/java/SeleniumTestingUsingPageObjectModel/Tests/BankTests.java
@@ -0,0 +1,16 @@
+package SeleniumTestingUsingPageObjectModel.Tests;
+
+import PageObjectModel.BankPage;
+import SeleniumTestingUsingPageObjectModel.Base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BankTests extends BaseTest {
+
+    @Test
+    public void testBankNameIsDisplayed() throws InterruptedException {
+        BankPage bankPage = loginPage.LoginIntoApplication("admin", "admin123");
+        Thread.sleep(1000);
+        Assert.assertTrue(bankPage.isBankNameDisplayed(), "Bank Name is not visible, Login might have failed");
+    }
+}
